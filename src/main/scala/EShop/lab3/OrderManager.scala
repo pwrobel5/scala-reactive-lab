70507c582fefa292523a5f71ac8ae913f4bede58
Piotr Wróbel <pwrobel5@gmail.com>
2020-11-24 23:35:24 +0100

    added templates from task 2

---
+++ src/main/scala/EShop/lab3/OrderManager.scala
@@ -0,0 +1,40 @@
+package EShop.lab3
+
+import akka.actor.{Actor, ActorRef}
+
+object OrderManager {
+
+  sealed trait Command
+  case class AddItem(id: String)                                               extends Command
+  case class RemoveItem(id: String)                                            extends Command
+  case class SelectDeliveryAndPaymentMethod(delivery: String, payment: String) extends Command
+  case object Buy                                                              extends Command
+  case object Pay                                                              extends Command
+  case class ConfirmCheckoutStarted(checkoutRef: ActorRef)                     extends Command
+  case class ConfirmPaymentStarted(paymentRef: ActorRef)                       extends Command
+  case object ConfirmPaymentReceived                                           extends Command
+
+  sealed trait Ack
+  case object Done extends Ack //trivial ACK
+}
+
+class OrderManager extends Actor {
+
+  override def receive: Receive = uninitialized
+
+  def uninitialized: Receive = ???
+
+  def open(cartActor: ActorRef): Receive = ???
+
+  def inCheckout(cartActorRef: ActorRef, senderRef: ActorRef): Receive = ???
+
+  def inCheckout(checkoutActorRef: ActorRef): Receive = ???
+
+  def inPayment(senderRef: ActorRef): Receive = ???
+
+  def inPayment(paymentActorRef: ActorRef, senderRef: ActorRef): Receive = ???
+
+  def finished: Receive = {
+    case _ => sender ! "order manager finished job"
+  }
+}
